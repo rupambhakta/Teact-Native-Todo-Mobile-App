c618064a9043f5e225089d544549f287fc923ae5
Rupam Bhakta <rupambhakta2020@gmail.com>
2025-08-07 00:40:38 +0530

    Style added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,25 @@
-import useTheme from "@/hooks/useTheme";
+import { createHomeStyles } from "@/assets/style/home.style";
+import { api } from "@/convex/_generated/api";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { Link } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import Header from "@/components/Header";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyle = createHomeStyles(colors);
+
   return (
-    <View style={style.container}>
-      <Text>This will be my first react native </Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle the mode</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyle.container}>
+      <StatusBar barStyle={colors.statusBarStyle}/>
+      <Header/>
+      <SafeAreaView style={homeStyle.safeArea}>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle the mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  
-});
